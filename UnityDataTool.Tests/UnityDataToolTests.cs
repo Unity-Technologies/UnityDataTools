f9814466d7b72818ac74e9a1ceef2a192e1985e0
Andrew Skowronski <andrew.skowronski@unity3d.com>
2025-12-18 15:17:23 -0500

    Split WebBundle testing into its own class
    The UnityDataToolTests test class repeats the test through each folder in Data/AssetBundles, but webbundle tests are covering a file in Data/WebBundles and only need to run once.

--- UnityDataTool.Tests/UnityDataToolTests.cs
+++ UnityDataTool.Tests/UnityDataToolTests.cs
@@ -12,11 +12,11 @@ namespace UnityDataTools.UnityDataTool.Tests;
 
 #pragma warning disable NUnit2005, NUnit2006
 
-public class UnityDataToolTests : AssetBundleTestFixture
+public class UnityDataToolAssetBundleTests : AssetBundleTestFixture
 {
     private string m_TestOutputFolder;
 
-    public UnityDataToolTests(Context context) : base(context)
+    public UnityDataToolAssetBundleTests(Context context) : base(context)
     {
     }
 
@@ -59,20 +59,6 @@ public class UnityDataToolTests : AssetBundleTestFixture
         Assert.AreNotEqual(0, await Program.Main(command.ToArray()));
     }
 
-    public void IsWebBundle_True()
-    {
-
-        var webBundlePath = Path.Combine(Context.TestDataFolder, "WebBundles", "HelloWorld.data");
-        Assert.IsTrue(Archive.IsWebBundle(new FileInfo(webBundlePath)));
-    }
-
-    [Test]
-    public void IsWebBundle_False()
-    {
-        var nonWebBundlePath = Path.Combine(Context.TestDataFolder, "WebBundles", "NotAWebBundle.txt");
-        Assert.IsFalse(Archive.IsWebBundle(new FileInfo(nonWebBundlePath)));
-    }
-
     [Test]
     public async Task ArchiveExtract_AssetBundle_FilesExtractedSuccessfully(
         [Values("", "-o archive", "--output-path archive")] string options)
@@ -83,27 +69,6 @@ public class UnityDataToolTests : AssetBundleTestFixture
         Assert.IsTrue(File.Exists(Path.Combine(m_TestOutputFolder, "archive", "CAB-5d40f7cad7c871cf2ad2af19ac542994")));
         Assert.IsTrue(File.Exists(Path.Combine(m_TestOutputFolder, "archive", "CAB-5d40f7cad7c871cf2ad2af19ac542994.resS")));
         Assert.IsTrue(File.Exists(Path.Combine(m_TestOutputFolder, "archive", "CAB-5d40f7cad7c871cf2ad2af19ac542994.resource")));
-    }
-
-    [Test]
-    public async Task ArchiveExtract_WebBundle_FileExtractedSuccessfully(
-        [Values("", "-o archive", "--output-path archive")] string options,
-        [Values("HelloWorld.data", "HelloWorld.data.gz", "HelloWorld.data.br")] string bundlePath)
-    {
-        var path = Path.Combine(Context.TestDataFolder, "WebBundles", bundlePath);
-        string[] expectedFiles = {
-            "boot.config",
-            "data.unity3d",
-            "RuntimeInitializeOnLoads.json",
-            "ScriptingAssemblies.json",
-            Path.Combine("Il2CppData", "Metadata", "global-metadata.dat"),
-            Path.Combine("Resources", "unity_default_resources"),
-        };
-        Assert.AreEqual(0, await Program.Main(new string[] { "archive", "extract", path }.Concat(options.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToArray()));
-        foreach (var file in expectedFiles)
-        {
-            Assert.IsTrue(File.Exists(Path.Combine(m_TestOutputFolder, "archive", file)));
-        }
     }
 
     [Test]
@@ -132,54 +97,6 @@ public class UnityDataToolTests : AssetBundleTestFixture
             Assert.AreEqual($"  Size: {Context.ExpectedData.Get("CAB-5d40f7cad7c871cf2ad2af19ac542994.resource-Size")}", lines[9]);
             Assert.AreEqual($"  Flags: {(ArchiveNodeFlags)(long)Context.ExpectedData.Get("CAB-5d40f7cad7c871cf2ad2af19ac542994.resource-Flags")}", lines[10]);
 
-        }
-        finally
-        {
-            Console.SetOut(currentOut);
-        }
-    }
-
-    [Test]
-    public async Task ArchiveList_WebBundle_ListFilesCorrectly(
-         [Values(
-            "HelloWorld.data",
-            "HelloWorld.data.gz",
-            "HelloWorld.data.br"
-        )] string bundlePath)
-    {
-        var path = Path.Combine(Context.TestDataFolder, "WebBundles", bundlePath);
-        using var sw = new StringWriter();
-        var currentOut = Console.Out;
-        try
-        {
-            Console.SetOut(sw);
-
-            Assert.AreEqual(0, await Program.Main(new string[] { "archive", "list", path }));
-
-            var actualOutput = sw.ToString();
-            var expectedOutput = (
-@"data.unity3d
-  Size: 253044
-
-RuntimeInitializeOnLoads.json
-  Size: 700
-
-ScriptingAssemblies.json
-  Size: 3060
-
-boot.config
-  Size: 93
-
-Il2CppData/Metadata/global-metadata.dat
-  Size: 1641180
-
-Resources/unity_default_resources
-  Size: 607376
-
-"
-            );
-
-            Assert.AreEqual(expectedOutput, actualOutput);
         }
         finally
         {
@@ -434,5 +351,115 @@ public class UnityDataToolPlayerDataTests : PlayerDataTestFixture
         expected = Regex.Replace(expected, @"\r\n|\n\r|\r", "\n");
 
         Assert.AreEqual(expected, content);
+    }
+}
+
+public class WebBundleSupportTests
+{
+    private string m_TestOutputFolder;
+    private string m_TestDataFolder;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        m_TestOutputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "test_folder");
+        m_TestDataFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data");
+        Directory.CreateDirectory(m_TestOutputFolder);
+        Directory.SetCurrentDirectory(m_TestOutputFolder);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        SqliteConnection.ClearAllPools();
+
+        var testDir = new DirectoryInfo(m_TestOutputFolder);
+        testDir.EnumerateFiles()
+            .ToList().ForEach(f => f.Delete());
+        testDir.EnumerateDirectories()
+            .ToList().ForEach(d => d.Delete(true));
+    }
+
+    [Test]
+    public void IsWebBundle_True()
+    {
+        var webBundlePath = Path.Combine(m_TestDataFolder, "WebBundles", "HelloWorld.data");
+        Assert.IsTrue(Archive.IsWebBundle(new FileInfo(webBundlePath)));
+    }
+
+    [Test]
+    public void IsWebBundle_False()
+    {
+        var nonWebBundlePath = Path.Combine(m_TestDataFolder, "WebBundles", "NotAWebBundle.txt");
+        Assert.IsFalse(Archive.IsWebBundle(new FileInfo(nonWebBundlePath)));
+    }
+
+    [Test]
+    public async Task ArchiveList_WebBundle_ListFilesCorrectly(
+         [Values(
+            "HelloWorld.data",
+            "HelloWorld.data.gz",
+            "HelloWorld.data.br"
+        )] string bundlePath)
+    {
+        var path = Path.Combine(m_TestDataFolder, "WebBundles", bundlePath);
+        using var sw = new StringWriter();
+        var currentOut = Console.Out;
+        try
+        {
+            Console.SetOut(sw);
+
+            Assert.AreEqual(0, await Program.Main(new string[] { "archive", "list", path }));
+
+            var actualOutput = sw.ToString();
+            var expectedOutput = (
+@"data.unity3d
+  Size: 253044
+
+RuntimeInitializeOnLoads.json
+  Size: 700
+
+ScriptingAssemblies.json
+  Size: 3060
+
+boot.config
+  Size: 93
+
+Il2CppData/Metadata/global-metadata.dat
+  Size: 1641180
+
+Resources/unity_default_resources
+  Size: 607376
+
+"
+            );
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+        finally
+        {
+            Console.SetOut(currentOut);
+        }
+    }
+
+    [Test]
+    public async Task ArchiveExtract_WebBundle_FileExtractedSuccessfully(
+        [Values("", "-o archive", "--output-path archive")] string options,
+        [Values("HelloWorld.data", "HelloWorld.data.gz", "HelloWorld.data.br")] string bundlePath)
+    {
+        var path = Path.Combine(m_TestDataFolder, "WebBundles", bundlePath);
+        string[] expectedFiles = {
+            "boot.config",
+            "data.unity3d",
+            "RuntimeInitializeOnLoads.json",
+            "ScriptingAssemblies.json",
+            Path.Combine("Il2CppData", "Metadata", "global-metadata.dat"),
+            Path.Combine("Resources", "unity_default_resources"),
+        };
+        Assert.AreEqual(0, await Program.Main(new string[] { "archive", "extract", path }.Concat(options.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToArray()));
+        foreach (var file in expectedFiles)
+        {
+            Assert.IsTrue(File.Exists(Path.Combine(m_TestOutputFolder, "archive", file)));
+        }
     }
 }
